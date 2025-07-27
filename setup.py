f8544b65fd9c7702f47bf2b940b4d5e53e37bf1c
Ryan Jasinski <jasinski.17@gmail.com>
2025-07-26 17:11:49 -0700

    barebones of inputs

---
+++ setup.py
@@ -0,0 +1,16 @@
+# This script can be run to interactively setup the DB contianer through the terminal
+import subprocess
+
+# Inputs needed
+# # PostgreSQL
+# POSTGRES_USER=
+# POSTGRES_PASSWORD=
+# POSTGRES_DB=
+# POSTGRES_CONTAINER_NAME=
+# POSTGRES_PORT=
+
+# # pgAdmin
+# PGADMIN_DEFAULT_EMAIL=
+# PGADMIN_DEFAULT_PASSWORD=
+# PGADMIN_PORT=
+
